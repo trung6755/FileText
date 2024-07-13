e5956f4ce05f5cd09bfd2cf4941fcc01a6b41645
trung6755 <trungggrr@gmail.com>
2024-07-13 21:06:38 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input file path");
+            string path = Console.ReadLine();
+            Console.WriteLine("File path: " + path);
+            ReadFileText example = new ReadFileText();
+            example.ReadTextFile(path);
+        }
+    }
+}
---
+++ ReadFileText.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System;
+
+namespace FileText
+{
+    class ReadFileText
+    {
+        public void ReadTextFile(string filePath)
+        {
+            try
+            {
+                FileInfo file = new FileInfo(filePath);
+                if (!file.Exists)
+                {
+                    throw new FileNotFoundException();
+                }
+
+                StreamReader reader = new StreamReader(filePath);
+                string line = "";
+                int sum = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    sum += Int32.Parse(line);
+                }
+                reader.Close();
+                Console.WriteLine("Total: " + sum);
+            }
+            catch (System.Exception)
+            {
+                Console.Error.WriteLine("File not found or invalid content");
+            }
+        }
+    }
+}
